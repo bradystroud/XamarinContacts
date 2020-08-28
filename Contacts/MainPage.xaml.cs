114056829eac8c7c70a93b15e596450e66df14d7
Brady Stroud <38869720+bradystroud@users.noreply.github.com>
2020-08-29 07:46:08 +1000

    initial commit

---
+++ Contacts/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Contacts {
+  public partial class MainPage : ContentPage {
+    public MainPage()
+    {
+      InitializeComponent();
+    }
+
+    private void SaveButton_Pressed(object sender, EventArgs e)
+    {
+      Console.WriteLine("Save Button pressed");
+      Console.WriteLine(NameEntry.Text);
+    }
+
+    private void NameEntry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+      BindingContext = this;
+
+      Console.WriteLine(NameEntry.Text);
+      string previewText = NameEntry.Text; //Trying to change the value of a label on the go
+    }
+  }
+}
